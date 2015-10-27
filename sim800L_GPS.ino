f4019e7d52e626fbab88f260954b0103cd8e8e7a
sibonjic.haris@gmail.com <sibonjic.haris@gmail.com>
2015-10-27 15:37:29 +0100

    finished version

--- sim800L_GPS.ino
+++ sim800L_GPS.ino
@@ -1,47 +1,73 @@
 #include <TinyGPS.h>
 #include <SoftwareSerial.h>
 #include "inetGSM.h"
+#include "gps.h"
 
 SoftwareSerial ss(11, 12);
-//TinyGPS gps;
+TinyGPS gps;
 
 InetGSM inet;
+GPSGSM gpsSIM;
 
 int numdata;
 char msg[50];
 
 void setup(){
+  
   Serial.begin(9600);
   ss.begin(9600);
+  Serial.println("starting SIM800L");
 }
 
 float flat, flon;
 unsigned long fix_age;
 
 void loop(){
-  delay(1999);
-
-  sendPost();
+  //delay(1999);
+  //getTime();
+  //sendPost();
      //Read for new byte on serial hardware,
      //and write them on NewSoftSerial.
      //serialhwread();
      //Read for new byte on NewSoftSerial.
      //serialswread();
 
-     //smartdelay(1000);
+   smartdelay(1000);
+   gps.f_get_position(&flat, &flon, &fix_age);
+   Serial.println(flon);
+   Serial.println(flat);
+
+  
+   if (flon < 1000.00)
+     sendPost();
+   
 };
 
-//static void smartdelay(unsigned long ms)
-//{
-//  unsigned long start = millis();
-//  do 
-//  {
-//    while (ss.available())
-//      gps.encode(ss.read());
-//  } while (millis() - start < ms);
+static void smartdelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
+
+//void getTime(){
+//
+//  
 //}
 
 void sendPost(){
+  char str_flon[12];
+  dtostrf(flon, 9, 7, str_flon);
+  
+  char str_flat[11];
+  dtostrf(flat, 9, 7, str_flat);
+
+  Serial.println(str_flat);
+  Serial.println(str_flon);
+  
   boolean started=false;
   if (gsm.begin(2400)) {
     Serial.println("\nstatus=READY");
@@ -49,7 +75,8 @@ void sendPost(){
   } else Serial.println("\nstatus=IDLE");
   char msgBuffer[100];
   memset(msgBuffer , 0 , 100);
-  sprintf(msgBuffer , "[{ \"value\": {\"lng\": 95.3396,\"lat\": 55.3904},\"at\": \"2015-10-27T12:19:50.642+0000\"}]");
+  sprintf(msgBuffer , "[{ \"value\": {\"lng\": %s,\"lat\": %s},\"at\": \"2015-10-27T12:19:50.642+0000\"}]",str_flon,str_flat);
+  Serial.println(msgBuffer);
   if(started) {
     
     //GPRS attach, put in order APN, username and password.
